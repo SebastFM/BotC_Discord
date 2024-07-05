32dd3e09023c6a29fa39ee87c6d653fe2369e0df
SebastFM <sebastian.f.murillo@gmail.com>
2024-07-05 10:29:11 -0500

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,13 +1,34 @@
-
 import random
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
 
-    for i in range(pass_length):
-        password += random.choice(elements)
 
-    return password
 
-print(gen_pass(10)) 
+emodji = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+
+
+@client.event
+async def on_message(message):
+    emodji = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+    monde = ["Cara","Cruz"]
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('$emoji'):
+        await message.channel.send(random.choice(emodji))
+    elif message.content.startswith('$moneda'):
+        await message.channel.send(random.choice(monde))
